1467184e4e2583d4bdd60e1305442f7bc3b91ce4
Monika Dembek <monika.dembek@gmail.com>
2019-09-09 20:55:15 +0200

    Updates

---
+++ precache-manifest.3cebd23a57b101285e031a24cecec629.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f15c6be4a8dcfeef8dd8d6c749dca7",
+    "url": "/portfolio-junior-frontend-react/index.html"
+  },
+  {
+    "revision": "93934f28d0c23f5efda3",
+    "url": "/portfolio-junior-frontend-react/static/css/main.8afde091.chunk.css"
+  },
+  {
+    "revision": "1e4a6e1dc12ea2798ca2",
+    "url": "/portfolio-junior-frontend-react/static/js/2.687cdee6.chunk.js"
+  },
+  {
+    "revision": "93934f28d0c23f5efda3",
+    "url": "/portfolio-junior-frontend-react/static/js/main.a39085d0.chunk.js"
+  },
+  {
+    "revision": "16b9c2e18f232dc817b7",
+    "url": "/portfolio-junior-frontend-react/static/js/runtime~main.b804f93e.js"
+  }
+]);
