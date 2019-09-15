1179aa6755b09cd1c8061e611c94f1906851f40e
Monika Dembek <monika.dembek@gmail.com>
2019-09-15 23:10:25 +0200

    add info about work in progress

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -3,18 +3,19 @@ import '../styles/StartPage.scss';
 
 const StartPage = () => {
     return (
-        <div>
-            <div className="intro1">
+        <div className="start">
+            <div className="start__intro--1">
                 <div>design</div>
                 <div>code</div>
                 <div>passion</div>
             </div>
-            <div className="intro2">
+            <div className="start__intro--2">
                 <h3>Monika.Dembek</h3>
                 <h2>Frontend</h2>
                 <h2>Developer</h2>
             </div>
-        </div>
+            <div style={{ marginTop: 40 }}>This is a new portfolio coded in React and it is still under contruction. <br />Most pages have its content but there are still some work to be done, especially implementing responsive web design.</div>
+        </div >
     );
 }
 
